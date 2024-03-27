83b9af49418bd843ee9adf6811dfc359e67a3811
ArnoldMajor <arnoldmwangakala@gmail.com>
2024-03-27 18:14:37 +0300

    solution control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let rides;
+
+function scuberGreetingForFeet(rides){
+  if (rides <= 400) {
+    return ('This one is on me!');
+  }
+
+  else if (rides > 401 && rides <= 2000) {
+    return ('That will be twenty bucks.');
+  }
+
+  else if (rides > 2000 && rides <= 2500) {
+    return ('I will gladly take your thirty bucks.');
+  }
+
+  else (rides > 2500); {
+    return ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city;
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const tipAmount = 'generous';
+
+let response;
+
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount) {
+    case 'generous':
+      return response = 'Thank you so much.';
+
+    case 'not as generous':
+      return response = 'Thank you.';
+
+    default:
+      return response = 'Bye.';
+  }
+  
 }
+
